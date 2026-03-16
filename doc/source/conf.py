1ee918f675fd7bb738cf43afd3cab6b13038167d
Brian Kohan <bckohan@gmail.com>
2026-03-16 11:36:40 -0700

    fix docs

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,8 +1,7 @@
 import sys
 from pathlib import Path
 
-sys.path.insert(0, str(Path(__file__).parent.parent.parent / "src"))
-sys.path.insert(0, str(Path(__file__).parent.parent.parent / "tests"))
+sys.path.insert(0, str(Path(__file__).parent.parent.parent))
 
 import django_snapshots
 
