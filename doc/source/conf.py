ba9c896d2e100252785c205079ea6fc840dad164
Brian Kohan <bckohan@gmail.com>
2026-03-16 10:56:21 -0700

    fix docs build

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -3,6 +3,7 @@ import sys
 from pathlib import Path
 
 sys.path.insert(0, str(Path(__file__).parent.parent.parent / "src"))
+sys.path.insert(0, str(Path(__file__).parent.parent.parent / "tests"))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.settings")
 
